3a2e773c8df555c8ce5b17be85d99a33a4bfe2c9
RenegadeAsturias <renegade.asturias@gmail.com>
2023-05-30 01:53:17 +0200

    Actualización del repositorio del Curso Profesional Angular: Proyecto Final

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from 'src/app/interfaces/card.interfaces';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
+  @Input() card!:Card;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
 }
